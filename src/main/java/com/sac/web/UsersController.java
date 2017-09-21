e21906eae80590072002a7193888201cd0aafdbe
easonwwg <990793233@qq.com>
2017-09-21 22:24:05 +0800

    first commit

---
+++ src/main/java/com/sac/web/UsersController.java
@@ -0,0 +1,33 @@
+package com.sac.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by 99079 on 2017/9/21.
+ */
+
+@Controller
+@RequestMapping(value = "/users")
+public class UsersController {
+
+    private static  final Logger logger= LoggerFactory.getLogger(UsersController.class);
+
+    /**
+     * 跳转到查询所有角色的页面
+     * @return
+     */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public String list(Model model){
+       // List<Role> roleList = roleService.list();
+       // model.addAttribute("roleList",roleList);
+        System.out.print("ok");
+        return "main";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 99079
+  Date: 2017/9/21
+  Time: 21:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/jquery-1.7.1.js"></script>
+</head>
+<body>
+sac
+</body>
+<script>
+    $(document).ready(function () {
+        alert("hello");
+    });
+
+</script>
+</html>
