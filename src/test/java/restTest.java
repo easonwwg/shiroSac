bca84ececfa240b6119a3eb46dbfe74130649b6e
unknown <>
2017-09-22 15:39:32 +0800

    测试类提交

---
+++ src/test/java/restTest.java
@@ -0,0 +1,32 @@
+import com.sac.service.system.Interface.ITestQueryService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
+
+/**
+ * Created by EAISON on 2017/9/22.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/applicationContext-service.xml",
+        "classpath:spring/spring-mybatis.xml"})
+public class restTest {
+
+    @Autowired
+    ITestQueryService service;
+
+    @Test
+    @Transactional   //标明此方法需使用事务
+    @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
+    public  void  GetNames(){
+
+        System.out.print(service.GetAllUserName()
+
+        );
+    }
+
+}
