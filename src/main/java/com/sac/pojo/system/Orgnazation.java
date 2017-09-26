d40bba57f672ff21fb61c28239add2c0732c7a1b
easonwwg <990793233@qq.com>
2017-09-26 14:12:04 +0800

    提交实体类

---
+++ src/main/java/com/sac/pojo/system/Orgnazation.java
@@ -0,0 +1,96 @@
+package com.sac.pojo.system;
+
+/**
+ * Created by EAISON on 2017/9/26.
+ */
+public class Orgnazation {
+    private int id;
+
+    private  String name;
+
+    private String shortName;
+
+    private  String code;
+
+    private  String orgType;
+
+    private String parentCode;
+
+    private  String state;
+
+    private String desc;
+
+    private String sort;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getOrgType() {
+        return orgType;
+    }
+
+    public void setOrgType(String orgType) {
+        this.orgType = orgType;
+    }
+
+    public String getParentCode() {
+        return parentCode;
+    }
+
+    public void setParentCode(String parentCode) {
+        this.parentCode = parentCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+}
