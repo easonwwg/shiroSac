5b1487e59dfa1f4ee0c77521e6b1f2f25e4225ff
easonwwg <990793233@qq.com>
2017-09-28 23:15:40 +0800

    鹽

---
+++ src/main/java/com/sac/commons/Md5Tools.java
@@ -0,0 +1,15 @@
+package com.sac.commons;
+
+import org.apache.shiro.crypto.hash.SimpleHash;
+
+/**
+ * Created by 99079 on 2017/9/28.
+ */
+public class Md5Tools {
+
+    public  static String  GetMd5(String str,String pwd,String salt,int
+                                  count){
+        SimpleHash simpleHash=new SimpleHash(str,pwd,salt,count);
+        return  simpleHash.toString();
+    }
+}
