b24aa3ab7ec666d908c508139c6297f8cf89bef7
easonwwg <990793233@qq.com>
2017-09-28 10:29:55 +0800

    权限服务

---
+++ src/main/java/com/sac/service/system/Impl/ResourceServiceImpl.java
@@ -0,0 +1,9 @@
+package com.sac.service.system.Impl;
+
+import com.sac.service.system.Interface.ResourceService;
+
+/**
+ * Created by EAISON on 2017/9/28.
+ */
+public class ResourceServiceImpl  implements ResourceService{
+}
---
+++ src/main/java/com/sac/service/system/Impl/RoleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.sac.service.system.Impl;
+
+import com.sac.service.system.Interface.RoleService;
+
+/**
+ * Created by EAISON on 2017/9/28.
+ */
+public class RoleServiceImpl implements RoleService {
+}
