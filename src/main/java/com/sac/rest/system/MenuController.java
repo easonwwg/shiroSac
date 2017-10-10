ff93d0fc5b65b1ff2f130b710f8e25601103ed9f
easonwwg <990793233@qq.com>
2017-10-10 21:44:52 +0800

    服务接口

---
+++ src/main/java/com/sac/rest/system/MenuController.java
@@ -0,0 +1,27 @@
+package com.sac.rest.system;
+
+import com.sac.pojo.system.Menu;
+import com.sac.service.system.Interface.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by 99079 on 2017/10/10.
+ */
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/menus")
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/getMenus")
+    @ResponseBody
+    private Menu GetMenuLists(){
+        return  menuService.GetMenuList();
+    }
+
+
+}
