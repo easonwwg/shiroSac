77b1c0d75fb03565c72814f3bb2f1bc8cefced4c
easonwwg <990793233@qq.com>
2017-10-10 21:26:27 +0800

    递归提交

--- src/main/java/com/sac/service/system/Impl/ResourceServiceImpl.java
+++ src/main/java/com/sac/service/system/Impl/ResourceServiceImpl.java
@@ -1,9 +1,11 @@
 package com.sac.service.system.Impl;
 
 import com.sac.service.system.Interface.ResourceService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by EAISON on 2017/9/28.
  */
+@Service
 public class ResourceServiceImpl  implements ResourceService{
 }
---
+++ src/main/java/com/sac/service/system/Interface/MenuService.java
@@ -0,0 +1,12 @@
+package com.sac.service.system.Interface;
+
+import com.sac.pojo.system.Menu;
+
+import java.util.List;
+
+/**
+ * Created by 99079 on 2017/10/10.
+ */
+public interface MenuService {
+    Menu GetMenuList();
+}
