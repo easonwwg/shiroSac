8e3f01db7e3acae65c1ff02b3fcab9caac1198b0
easonwwg <990793233@qq.com>
2017-11-07 16:04:47 +0800

    严格按照阿里代码规范整合代码

--- src/main/java/com/sac/aop/ControllerMethodInterceptor.java
+++ src/main/java/com/sac/aop/ControllerMethodInterceptor.java
@@ -1,6 +1,5 @@
 package com.sac.aop;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
@@ -12,7 +11,7 @@ import java.util.Arrays;
 import java.util.Date;
 
 /**
- * @Author:eason
+ * @author:eason
  * @Description：定义控制器的方法拦截,保存日志
  * @Date: 11:07,2017/11/6
  * @ModifiedBy
@@ -20,25 +19,27 @@ import java.util.Date;
 public class ControllerMethodInterceptor implements MethodInterceptor {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private final static ObjectMapper jsonMapper = new ObjectMapper();
+    private  final  String restPackageName="com.sac.rest";
+    private  final  String webPackageName="com.sac.rest";
+    private  final  String login="login";
+    private  final  String  logOut="toLogOut";
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Object result = null;
         logger.error("环绕前: interceptor name: {}", invocation.getMethod().getName());
         Object[] methodsArgs = invocation.getArguments();
-        if (invocation.getMethod().getDeclaringClass().getPackage().getName().contains("com.sac.rest")) {
+        if (invocation.getMethod().getDeclaringClass().getPackage().getName().contains(restPackageName)) {
             System.out.println("记录用户请求接口的传入和返回的参数");
-        } else if (invocation.getMethod().getDeclaringClass().getPackage().getName().contains("com.sac.web")) {
+        } else if (invocation.getMethod().getDeclaringClass().getPackage().getName().contains(webPackageName)) {
             Object obj = Arrays.stream(methodsArgs).filter(s ->
                             s instanceof HttpServletRequest
             ).findAny().orElse(null);
             if (obj != null) {
                 HttpServletRequest request = (HttpServletRequest) obj;
-                if (invocation.getMethod().getName().equals("login")) {
+                if (login.equals(invocation.getMethod().getName())) {
                     System.out.println("登陆时间是" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-                } else if (invocation.getMethod().getName().equals("toLogOut")) {
+                } else if (logOut.equals(invocation.getMethod().getName())) {
                     System.out.println("登出时间是" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
                 }
                 System.out.println("用户请求的ip是" + request.getRemoteAddr());
--- src/main/java/com/sac/aop/ExceptionHandler.java
+++ src/main/java/com/sac/aop/ExceptionHandler.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Author:eason
- * @Description：
+ * @author:eason
+ * @Description：通用异常接口 dao serivce controller抛出的异常可以在这直接捕获
  * @Date: 14:05,2017/11/7
  * @ModifiedBy
  */
@@ -25,12 +26,15 @@ public class ExceptionHandler implements HandlerExceptionResolver {
         Map<String, Object> map = new HashMap<String, Object>();
         logger.error(ex.getMessage(), ex);
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/sysError");//跳转到自定义错误页面
+        //跳转到自定义错误页面
+        modelAndView.setViewName("/sysError");
         if (ex instanceof MaxUploadSizeExceededException) {
             modelAndView.addObject("msg", "文件大小超过限制!");
-            return modelAndView;
+        } else if (ex instanceof SQLException) {
+            modelAndView.addObject("msg", "数据库发生异常!");
+        } else {
+            map.put("msg", "系统错误!");
         }
-        map.put("msg", "系统错误!");
         return modelAndView;
     }
 }
